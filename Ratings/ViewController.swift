816e7b31e2d62448fc090d56a769427d0970bd13
ouyou58 <ouyou58@gmail.com>
2019-06-18 16:12:00 +0900

    向apple store中点击星星评分，用到了StoreKit框架，其中SKStoreReviewController.requestReview()会生成评分对话框。

--- Ratings/ViewController.swift
+++ Ratings/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reviewAction(_ sender: UIButton) {
+        
+        SKStoreReviewController.requestReview()
+    }
+    
 }
 
